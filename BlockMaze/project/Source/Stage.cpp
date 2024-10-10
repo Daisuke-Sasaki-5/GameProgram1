3363c137a028f01dd8112e87b4b3280e1758fece
Daisuke <std2495gp0006@edu.sca.ac.jp>
2024-10-10 11:48:11 +0900

    10/10 終わり

--- BlockMaze/project/Source/Stage.cpp
+++ BlockMaze/project/Source/Stage.cpp
@@ -1,5 +1,19 @@
 #include "Stage.h"
 
+const int HEIGHT = 8;
+const int WIDTH =  12;
+const int CHIP_SIZE = 40;
+int map[HEIGHT][WIDTH] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
 Stage::Stage()
 {
 	hImage = LoadGraph("data/image/parts.png");
@@ -11,5 +25,16 @@ Stage::~Stage()
 
 void Stage::Draw()
 {
-	DrawRectGraph(100, 100, 0, 40, 40, 40, hImage, TRUE);
+	for (int j = 0; j < HEIGHT; j++) {
+		int y = 100 + j * 40;
+		for (int i = 0; i < WIDTH; i++) {
+			int x = 100 + i * 40;
+			if (map[j][i] == 1) {
+				DrawRectGraph(x, y, 0, 40, CHIP_SIZE, CHIP_SIZE, hImage, TRUE);
+			}
+			if (map[j][i] == 2) {
+				DrawRectGraph(x, y, 120, 0, CHIP_SIZE, CHIP_SIZE, hImage, TRUE);
+			}
+		}
+	}
 }
